d2593678a69d1182b2ef917eecd24a9e2da2c00f
DennisTurco <dennisturco@gmail.com>
2026-02-16 22:16:25 +0100

    update for linux env

--- config/settings.py
+++ config/settings.py
@@ -2,5 +2,5 @@ import os
 
 MIGRATIONS_DIR = os.path.join("database", "migrations")
 
-PRODUCTION_DATABASE_DIR = os.path.join(os.environ["USERPROFILE"], "Documents", "Shard", "data")
+PRODUCTION_DATABASE_DIR = os.path.join(os.path.expanduser("~"), "Documents", "Shard", "data")
 PRODUCTION_DATABASE_PATH = os.path.join(PRODUCTION_DATABASE_DIR, "SupportTool.db")
